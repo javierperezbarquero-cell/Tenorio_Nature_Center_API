67a11968de18207c12fbf57b03dcca5e65bfdd9c
Yampier0206 <yamir92.juegos@gmail.com>
2026-05-01 15:01:45 -0600

    Agregue la clase de usuarios con sus funciones y el login

--- api/server.go
+++ api/server.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"rest/dto"
+	"rest/security"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -9,13 +10,20 @@ import (
 )
 
 type Server struct {
-	dbtx   *dto.Queries
-	router *gin.Engine
+	dbtx         *dto.Queries
+	router       *gin.Engine
+	tokenBuilder security.Builder
 }
 
-func NewServer(dbtx *dto.Queries) (*Server, error) {
+func NewServer(dbtx *dto.Queries, secret string) (*Server, error) {
+	builder, err := security.NewPasetoBuilder(secret)
+	if err != nil {
+		return nil, err
+	}
+
 	server := &Server{
-		dbtx: dbtx,
+		dbtx:         dbtx,
+		tokenBuilder: builder,
 	}
 	router := gin.Default()
 	router.Use(cors.Middleware(cors.Config{
@@ -38,6 +46,10 @@ func NewServer(dbtx *dto.Queries) (*Server, error) {
 	router.GET("api/v1/ubicacion/:id", server.getUbicacionById)
 	router.PUT("api/v1/ubicacion", server.updateUbicacion)
 	router.DELETE("api/v1/ubicacion/:id", server.deleteUbicacion)
+	router.POST("api/v1/login", server.login)
+	router.POST("api/v1/usuario", server.createUsuario)
+	router.PUT("api/v1/usuario", server.updateUsuario)
+	router.DELETE("api/v1/usuario/:id", server.deleteUsuario)
 
 	//RUTAS CON MIDDLEWARE
 
--- main.go
+++ main.go
@@ -20,12 +20,12 @@ func main() {
 		log.Fatal("Error, no se pudo conectar a la base de datos", err)
 	}
 	dbtx := dto.New(conn)
-	server, err := api.NewServer(dbtx)
+	server, err := api.NewServer(dbtx, config.Secret)
 	if err != nil {
 		log.Fatal("No se puede iniciar el servidor", err)
 	}
 	err = server.Start(config.ServerURL)
 	if err != nil {
-		log.Fatal("Error al fatal", err)
+		log.Fatal("Error al iniciar el servidor", err)
 	}
 }
